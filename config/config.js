32eabdc04d40ef169a4909a107257e9a92722b75
Fabian Zwemke <fabianzwemke@gmail.com>
2018-01-01 15:32:41 +0100

    Log if config element does not exist

--- config/config.js
+++ config/config.js
@@ -4,7 +4,16 @@ var config = io.loadConfig();
 var default_config = io.loadDefaultConfig();
 
 exports.get = function(key) {
-    return config.hasOwnProperty(key) ? config[key] : default_config[key];
+    if(config.hasOwnProperty(key)){
+      return config[key];
+    }
+    else if(default_config.hasOwnProperty(key)){
+      return default_config[key];
+    }
+    else{
+      console.log('[CONFIG] Could not find config object ' + key);
+      return undefined;
+    }
 }
 
 exports.set = function(key, object, save = true) {
